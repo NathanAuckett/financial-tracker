2c7668c3cfc93812139e0b1674c99ff4fc1c2597
Nathan <n.auckett@gmail.com>
2024-04-05 14:05:28 +1100

    Added bulk transaction insert route.

--- backend/src/controllers/controller_transactions.ts
+++ backend/src/controllers/controller_transactions.ts
@@ -5,15 +5,23 @@ const { Transaction } = require('../models/');
 import {query as queryGetDuplicateTransactions} from '../queries/query_transaction_get_duplicates';
 
 
-function createTransaction(req: Request, res: Response) {
+function addTransaction(req: Request, res: Response) { //Expects a transaction object
     Transaction.create(req.body);
 
     return res.status(201).json({
-        message: 'Transactions created successfully'
+        message: 'Single Transaction created successfully'
     });
 }
 
-async function getAllTransactions(req: Request, res: Response){
+function addBulkTransactions(req: Request, res: Response) { //expects and array of transaction objects
+    Transaction.bulkCreate(req.body);
+
+    return res.status(201).json({
+        message: 'Bulk Transactions created successfully'
+    });
+}
+
+async function getAllTransactions(req: Request, res: Response){ //Returns all transactions in the DB
     const users = await Transaction.findAll();
     
     return res.status(201).json({
@@ -22,7 +30,7 @@ async function getAllTransactions(req: Request, res: Response){
     });
 }
 
-async function getDuplicates(req: Request, res: Response){
+async function getDuplicates(req: Request, res: Response){ //returns duplicate transactions. Where, account id, credit, debit, purchase date and description all match exactly
     const [results, metadata] = await sq.query(queryGetDuplicateTransactions);
 
     return res.status(201).json({
@@ -32,10 +40,8 @@ async function getDuplicates(req: Request, res: Response){
 }
 
 module.exports = {
-    createTransaction,
-    getAllTransactions,
-    getDuplicates
-    createTransaction,
+    addTransaction,
+    addBulkTransactions,
     getAllTransactions,
     getDuplicates
 }
--- backend/src/routes/route_transactions.ts
+++ backend/src/routes/route_transactions.ts
@@ -3,7 +3,9 @@ const controller = require('../controllers/controller_transactions');
 
 const router = express.Router();
 
-router.post('/transaction', controller.createTransaction);
+router.post('/add-transaction', controller.addTransaction);
+
+router.post('/add-bulk-transactions', controller.addBulkTransactions);
 
 router.get("/", (req, res) => {
     res.send("insert transactions here");

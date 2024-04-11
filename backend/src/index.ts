d2674d8d65fd5761d02df5161056af48c3663af1
Nathan Auckett <n.auckett@gmail.com>
2024-04-11 20:57:05 +1000

    Began core layout of site and basic data display in table

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
 
 function serverStart(){
   const routerUsers = require("./routes/route_users");
